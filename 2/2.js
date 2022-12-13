1368d36c4486ac8fb6d27b204ff56efae0f5a5ed
Алексей Макаров <raminov.83@mail.ru>
2022-12-13 21:46:29 +0300

    first commit

---
+++ 2/2.js
@@ -0,0 +1,32 @@
+// JSON
+const jsonString = `
+{
+	"list": [
+	 {
+	  "name": "Petr",
+	  "age": "20",
+	  "prof": "mechanic"
+	 },
+	 {
+	  "name": "Vova",
+	  "age": "60",
+	  "prof": "pilot"
+	 }
+	]
+   }
+   `;
+
+//Получение данных
+let data = JSON.parse(jsonString);
+
+let list = data.list;
+
+let result = { list: [] };
+let objList = new Object();
+
+list.forEach(function (elem) {
+  objList = elem;
+
+  result.list.push(objList);
+});
+console.log(result);
